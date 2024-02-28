ec58fbf8b51a430c9b2894af2374cd1404050496
joey iny <joeyainy@gmail.com>
2024-02-27 19:15:13 -0700

    Remove unused tokens

--- frontend/lib/tokens.ts
+++ frontend/lib/tokens.ts
@@ -3,26 +3,7 @@ export const tokens = [
     symbol: "tFHE",
     name: "Fhenix Tokens",
     address: "0x9aabe640c6befd824c7775a939a45f1bc7a24a79",
-  },
-  {
-    symbol: "ETH",
-    name: "Ether",
-    address: "pending",
     image:
       "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/ZJZZK5B2ZNF25LYQHMUTBTOMLU.png",
-  },
-  {
-    symbol: "DAI",
-    name: "DAI",
-    address: "pending",
-    image:
-      "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/UVVKGPI6U5CDXFAIVCLHODHZ6M.png",
-  },
-  {
-    symbol: "USDC",
-    name: "USDC",
-    address: "pending",
-    image:
-      "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/7ZYCGPG5DFFPJNTEDNPUE75AJY.png",
   },
 ];
