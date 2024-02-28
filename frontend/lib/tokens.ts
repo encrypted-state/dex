a47e6d80cf3d48385ddb3d41a3c597bb08b2c0c2
joey iny <joeyainy@gmail.com>
2024-02-27 18:06:09 -0700

    Add tFHE token to tokens

--- frontend/lib/tokens.ts
+++ frontend/lib/tokens.ts
@@ -1,5 +1,10 @@
 export const tokens = [
   {
+    symbol: "tFHE",
+    name: "Fhenix Tokens",
+    address: "0x9aabe640c6befd824c7775a939a45f1bc7a24a79",
+  },
+  {
     symbol: "ETH",
     name: "Ether",
     address: "pending",
